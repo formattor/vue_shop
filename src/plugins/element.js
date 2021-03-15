044fa01d70abbd0a8e419bcb0dd80489245ac398
zhangyushi <1193822368@qq.com>
2021-03-15 13:37:09 +0800

    完成订单.其实没完成

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -7,7 +7,7 @@ import {
   Table, TableColumn, Switch, Tooltip, Pagination, Dialog,
   MessageBox, Tag, Tree, Select, Option,
   Cascader, Tabs, TabPane, Step, Steps,
-  Checkbox, CheckboxGroup, Upload
+  Checkbox, CheckboxGroup, Upload, Timeline, TimelineItem
 } from 'element-ui'
 
 Vue.use(Button)
@@ -52,6 +52,8 @@ Vue.use(Steps)
 Vue.use(Checkbox)
 Vue.use(CheckboxGroup)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 
 // 全局挂在到vue的原型对象上
 Vue.prototype.$message = Message
